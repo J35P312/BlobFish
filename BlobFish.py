95baada485695601769d3087ed1048b0b3ec2ad3
jemten <jemten@users.noreply.github.com>
2018-10-16 17:58:48 +0200

    sample --> condition, finds salmon file in dir

--- BlobFish.py
+++ BlobFish.py
@@ -2,7 +2,7 @@
 import argparse
 import os
 
-version = "0.0.1"
+version = "0.0.2"
 parser = argparse.ArgumentParser("""BlobFish-{}""".format(version),add_help=False)
 parser.add_argument('--allvsall' , help="Compare all individuals/samples/timepoints etc", required=False, action="store_true")
 args, unknown = parser.parse_known_args()
@@ -13,24 +13,24 @@ if args.allvsall:
 	parser = argparse.ArgumentParser("""BlobFish-{}""".format(version))
 	parser.add_argument('--allvsall' , help="Compare all individuals/samples/timepoints etc", required=False, action="store_true")
 	parser.add_argument('--paths', type=str,required=True,  help="path to salmon output directories (multiple directories are separated by \" \", eachdirecotry contain a quant.sf file", nargs='+')
-        parser.add_argument('--sample', type=str,required=True,  help="sample specification, the sample name for each input,separated by \" \", there should be one sample name for each input path", nargs='+')
+        parser.add_argument('--conditions', type=str,required=True,  help="Condition linked to each sample, separated by \" \", a condition must be given for each input path", nargs='+')
 	parser.add_argument('--dir', type=str,required=True,  help="The output is printed to this directory")
 	parser.add_argument('--tx', type=str,required=True,  help="The transcript to gene file")
 	args= parser.parse_args()
 
 	os.system("mkdir -p {}".format(args.dir))
-	if not len(args.paths) == len(args.sample):
-		print "Error: you need to specify one sample id for each input path"
+	if not len(args.paths) == len(args.conditions):
+		print "Error: you need to specify one condition for each input path"
 		quit()
 
 	args.wd=os.path.dirname(os.path.realpath(__file__))
 
 	sample_list={}
 	for i in range(0,len(args.paths)):
-		if not args.sample[i] in sample_list:
-			sample_list[args.sample[i]] = []
+		if not args.conditions[i] in sample_list:
+			sample_list[args.conditions[i]] = []
 
-		sample_list[args.sample[i]].append(args.paths[i])
+		sample_list[args.conditions[i]].append(os.path.join(args.paths[i], 'quant.sf'))
 
     
 	for a in sample_list:	
